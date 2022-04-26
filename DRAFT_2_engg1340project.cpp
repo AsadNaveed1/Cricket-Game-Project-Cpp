63a57a516953a145ef652bd4421e21647d5a8783
Firedrake2002 <sumerkaistha@gmail.com>
2022-04-27 01:50:23 +0530

    Refactored code, added struct Cricketer, added dynamic memory management, and added load, save_and_quit, and delete_array functions

--- DRAFT_2_engg1340project.cpp
+++ DRAFT_2_engg1340project.cpp
@@ -1,12 +1,41 @@
-#include<iostream>
+#include <iostream>
 #include <time.h>
-#include<string.h>
+#include <string>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
+const int NUM_INNINGS = 2;
+const int PRETTY_PRINT_NUM = 30;
+const char save_file_name[] = "cricket_save_file.txt";
 
+struct Cricketer {
+    int score;
+    int players_got_out;
+    int balls_thrown;
+
+    Cricketer() {
+        score = 0;
+        players_got_out = 0;
+        balls_thrown = 0; 
+    }
+
+    Cricketer(int score_, int players_got_out_, int balls_thrown_) {
+        score = score_;
+        players_got_out = players_got_out_;
+        balls_thrown = balls_thrown_;
+    }
+};
+
+void delete_cricketer_array(Cricketer** &cricketer_array, int size) {
+    for(int i = 0; i < size; i++) {
+        delete cricketer_array[i];
+    }
+    delete [] cricketer_array;
+}
 
 void game_name(){
     
@@ -22,8 +51,7 @@ void game_name(){
     cout << "|----------------------------------------------------------|\n" << endl;
 }
 
-void main_menu()
-{
+void main_menu() {
     cout << "\n\nMAIN MENU :-";
     cout << "\n\n1. Start a new game" << endl;
     cout << "\n2. Instructions" << endl;
@@ -31,8 +59,7 @@ void main_menu()
     cout << "\nEnter your decision: ";
     
 }
-void instructions()
-{
+void instructions() {
     cout << endl;
     cout << "INSTRUCTIONS AND RULES";
     cout << endl;
@@ -43,76 +70,67 @@ void instructions()
     cout << "5. One who scores more during their batting is declared as WINNER."<<endl;
     cout << "6. If both scores same, then the result will be a draw."<<endl;    
 }
-bool toss()
-{    
+
+void toss(bool &player_batting_bool) {    
     int coin_side_chosen, coin_side_landed;
 
     cout << "\n\nWELCOME TO TOSS!\n";
    
     cout << "\n Player 1 Choose your call:\n Please enter '1' for Heads and '2' for Tails ";
     cin >> coin_side_chosen;
-    coin_side_landed = rand()%2+1;
-    if(coin_side_landed == coin_side_chosen)
-    {
-        
+    coin_side_landed = rand() % 2 + 1;
+    bool toss_result;
+    int side_choice;
+
+    if(coin_side_landed == coin_side_chosen) {
         cout << "\n\n******************** You've won the Toss ********************" << endl;
-        return true;
-    }
-    else
-    {
-        
+        cout << "\nSelect your side:\n Please enter '1' for batting and '2' for bowling\n";
+        cin >> side_choice;
+    } else {
         cout << "\n\n******************** You've lost the Toss ********************" << endl;
-        return false;
+        side_choice = rand() % 2 + 1;
+    }
+
+    if(side_choice == 1) {
+        player_batting_bool = true;
+    } else if(side_choice = 2) {
+        player_batting_bool = false;
+    } else {
+        cout << "Please only enter '1' or '2' to select your choice";
     }
 }
 
-int player()
-{   
+int player() {   
     int res;
-    
     cout << "\nPlayer 1 Choose a number between 1 and 6: ";
-    while(1)
-    {
+    while(1) {
         cin >> res;
-        if(res>6||res<1)
-        {
-            
+        if(res>6||res<1) {   
             cout << "Please select a number between 1 and 6: ";
             continue;
-        }
-        else
-        {
+        } else {
             break;
         } 
     }
-
-
     return res;
 }
-int player2()
-{  
+int player2() {  
     int res;
     
     cout << "\nPlayer 2 Choose a number between 1 and 6: ";
-    while(1)
-    {
+    while(1) {
         cin >> res;
-        if(res>6||res<1)
-        {
-            
+        if(res>6||res<1) {
             cout << "Please select a number between 1 and 6: ";
             continue;
-        }
-        else 
-        {
+        } else {
             break;
         }    
     }
     return res;
 }
 
-int comp()
-{ 
+int comp() { 
     int res;
 
     cout << "\nPlayer 1 input a number to deliver a ball: ";
@@ -125,8 +143,7 @@ int comp()
     return res;
 }
 
-int comp2()
-{ 
+int comp2() { 
     int res;
 
     cout << "\nPlayer 2 input a number to ball: ";
@@ -139,99 +156,180 @@ int comp2()
     return res;
 }
 
-void start_game()
-{
+void save_and_quit_game(int &innings, int &overs, int &curr_ball, bool &player_batting_bool, int &user_num_cricketers, int &ai_num_cricketers, Cricketer** &user_cricketers_arr, Cricketer** &ai_cricketers_arr) {
+    ofstream fout;
+    fout.open(save_file_name);
+
+    if(fout.fail()) {
+        cout << "Error in file opening!" << endl;
+        exit(1);
+    }
+
+    // Save output to file
+    fout << user_num_cricketers << endl;
+    fout << ai_num_cricketers << endl;
+
+    for(int i = 0; i < user_num_cricketers; i++) {
+        fout << user_cricketers_arr[i]->score << ',' << user_cricketers_arr[i]->players_got_out << ',' << user_cricketers_arr[i]->balls_thrown << endl;
+    }
+
+    for(int i = 0; i < ai_num_cricketers; i++) {
+        fout << ai_cricketers_arr[i]->score << ',' << ai_cricketers_arr[i]->players_got_out << ',' << ai_cricketers_arr[i]->balls_thrown << endl;
+    }
+
+    fout << innings << endl;
+    fout << overs << endl;
+    fout << curr_ball << endl;
+    fout << player_batting_bool << endl;
+    
+    fout.close();
+    delete_cricketer_array(user_cricketers_arr, user_num_cricketers);
+    delete_cricketer_array(ai_cricketers_arr, ai_num_cricketers);
+}
+
+void initialize_game(char load_flag, int &innings, int &overs, int &curr_ball, bool &player_batting_bool, int &user_num_cricketers, int &ai_num_cricketers, Cricketer** &user_cricketers_arr, Cricketer** &ai_cricketers_arr) {
+    if(load_flag == 'n') {
+        innings = 1;
+        curr_ball = 0;
+        toss(player_batting_bool);
+
+        cout << "\nHow many Overs match would you like to have?" << endl;
+        cin >> overs;
+
+        cout << "How many cricketers would you like on your team? ";
+        cin >> user_num_cricketers;
+        
+        cout << "How many cricketers would you like on the opponent's team? ";
+        cin >> ai_num_cricketers;
+
+        user_cricketers_arr = new Cricketer*[user_num_cricketers];
+        ai_cricketers_arr = new Cricketer*[ai_num_cricketers];
+
+        for(int i = 0; i < user_num_cricketers; i++) {
+            user_cricketers_arr[i] = new Cricketer(0, 0, 0);
+        }
+
+        for(int i = 0; i < ai_num_cricketers; i++) {
+            ai_cricketers_arr[i] = new Cricketer(0, 0, 0);
+        }
+    } else {
+        ifstream fin;
+        fin.open(save_file_name);
+        
+        if(fin.fail()) {
+            cout << "Error in file opening!" << endl;
+            exit(1);
+        }
+
+        // Load game data from file
+        fin >> user_num_cricketers;
+        fin >> ai_num_cricketers;
+
+        user_cricketers_arr = new Cricketer*[user_num_cricketers];
+        ai_cricketers_arr = new Cricketer*[ai_num_cricketers];
+
+        for(int i = 0; i < user_num_cricketers; i++) {
+            string cricketer_vals;
+            fin >> cricketer_vals;
+            istringstream ss(cricketer_vals);
+            int vals_arr[] = {0, 0, 0};
+            for(int i = 0; i < 3; i++) {
+                string token;
+                getline(ss, token, ',');
+                vals_arr[i] = stoi(token);
+            }
+            user_cricketers_arr[i] = new Cricketer(vals_arr[0], vals_arr[1], vals_arr[2]);
+        }
+
+        for(int i = 0; i < ai_num_cricketers; i++) {
+            string cricketer_vals;
+            fin >> cricketer_vals;
+            istringstream ss(cricketer_vals);
+            int vals_arr[] = {0, 0, 0};
+            for(int i = 0; i < 3; i++) {
+                string token;
+                getline(ss, token, ',');
+                vals_arr[i] = stoi(token);
+            }
+            ai_cricketers_arr[i] = new Cricketer(vals_arr[0], vals_arr[1], vals_arr[2]);
+        }
+
+        fin >> innings;
+        fin >> overs;
+        fin >> curr_ball;
+        fin >> player_batting_bool;
+    }
+}
+
+void start_game() {
     game_name();
     srand(time(0));
     
     cout << "\n\n\n";
     
-    cout << "GAME STARTS!!!"<<endl;
+    cout << "GAME STARTS!!!" << endl;
+
+    int innings, overs, curr_ball;
+    bool player_batting_bool;
+    Cricketer** user_cricketers_arr;
+    Cricketer** ai_cricketers_arr;
+    int user_num_cricketers;
+    int ai_num_cricketers;
     
-    int side_choice, overs, balls, t1, t2;
-    int player_score=0, player2_score=0, b=0, first=0, second=0;  
-    string bat, bowl;    
+    char load_flag;
+    cout << "Do you want to load the game from the previous saved session? y/n" << endl;
+    cin >> load_flag;
+
+    while(load_flag != 'y' || load_flag != 'n') {
+        cout << "Please enter only y or n" << endl;
+        cin >> load_flag;
+    }
     
-    cout << "\nHow many Overs match would you like to have?";
-    cin >> overs;
-    balls = overs * 6;
-    bool toss_result = toss();
+    int t1, t2;
+    int player_score=0, player2_score=0,  first=0, second=0;
+    string bat="player";
+    string bowl="player2";
+
+    initialize_game(load_flag, innings, overs, curr_ball, player_batting_bool, user_num_cricketers, ai_num_cricketers, user_cricketers_arr, ai_cricketers_arr);
+
+    int balls = overs * 6;
+
     bool out = false;
-    if(toss_result)
-    {    
-        cout << "\nSelect your side:\n Please enter '1' for batting and '2' for bowling\n";
-        cin >> side_choice;
-    }
-    else
-    {
-        side_choice = rand()%2 + 1;
-    }
-    if(side_choice == 1)
-    {
-        bat = "Player";
-        bowl = "AI";
-    }
-    else if (side_choice = 2)
-    {
-        bowl = "Player";
-        bat = "AI";
-    }
-    else
-    {
-        cout << "Please only enter '1' or '2' to select your choice";
-    }
     
     cout << "\n\n";
    
     cout << "FIRST INNINGS";
     
-    if(bat=="player") 
-    {
+    if(bat=="player")  {
         cout << "\n\n---------------------------------Player 1 is Batting---------------------------------\n";
-        while((!out)&&(b<balls))
-        {   //First Innings
+        while((!out) && (curr_ball < balls)) {   //First Innings
             t1=player();
             t2=comp2();
-            if(t1==t2) 
-            {
+            if(t1==t2)  {
                 out = true;
-            
                 cout << "\nIt's OUT!!!\n";
-            }
-            else
-            {
+            } else {
                 player_score += t1;
-            
             }
-            b++;
+            curr_ball++;
         }
-
-    }    
-    else
-    {
+    } else {
         cout << "\n\n---------------------------------Player 2 is Batting---------------------------------\n";
-        while((!out)&&(b<balls))
-        {   //First Innings
+        while((!out)&&(curr_ball<balls)) {   //First Innings
             t1=player2();
             t2=comp();
-            if(t1==t2) 
-            {
+            if(t1==t2) {
                 out = true;
-            
                 cout << "\nIt's OUT!!!\n";
-            }
-            else
-            {
+            } else {
                 player2_score += t1;
             }
-            b++;
+            curr_ball++;
         }
-
     } 
 
     //Before second innings need to change bat and bal
-    if(bat=="player")
-    {
+    if(bat=="player") {
         bat = "player2";
         bowl = "player";
         first = player_score;
@@ -245,9 +343,7 @@ void start_game()
         cout << "SECOND INNINGS";
         
         cout << "\n\n---------------------------------Player 2 is Batting Now!!---------------------------------\n";
-    }
-    else
-    {
+    } else {
         bowl = "player2";
         bat = "player";
         first = player2_score;
@@ -264,59 +360,37 @@ void start_game()
     }
 
     out = false;
-    b=0;
+    curr_ball=0;
 
-    if(bat=="player")
-    {
-        while((!out)&&(b<balls)&&(second<=first))
-        {   //Second Innings
+    if(bat=="player") {
+        while((!out)&&(curr_ball<balls)&&(second<=first)) {   //Second Innings
             t1=player();
             t2=comp2();
-            if(t1==t2)
-            {
+            if(t1==t2) {
                 out = true;
-            
                 cout << "\nIt's OUT!!!\n";
-            } 
-            else
-            {
+            } else {
                 player_score += t1;
                 second+=t1;
-
             }
-            b++;
+            curr_ball++;
         }
-
-    }
-
-    else
-    {
-        while((!out)&&(b<balls)&&(second<=first))
-        {   //Second Innings
+    } else {
+        while((!out)&&(curr_ball<balls)&&(second<=first)) {   //Second Innings
             t1=player2();
             t2=comp();
-            if(t1==t2)
-            {
+            if(t1 == t2) {
                 out = true;
-            
                 cout << "\nIt's OUT!!!\n";
-            } 
-            else
-            {
- 
+            } else {
                 player2_score += t1;
                 second+=t1;
-            
             }
-            b++;
+            curr_ball++;
         }
     }
 
-    
-
-    if(bat=="player")
-    {
-        
+    if(bat=="player") {
         cout << "\nSecond Innings Ended!!\n";
        
         cout <<"\nPlayer 1 Score is "<< second <<endl;
@@ -326,25 +400,14 @@ void start_game()
         cout << "\n\n";
        
         cout << "RESULT";
-        if(first>second) 
-        {
-            
+        if(first>second)  {
             cout << "\n\n    Player 2 WON!!";
-        }
-        else if(first == second) 
-        {
-        
+        } else if(first == second)  {
             cout << "\n\nMatch Draw!!";
-        }
-        else
-        {
-           
+        } else {
             cout << "\n\n    Player 1 Won!!";
         }
-    }
-    else
-    {
-        
+    } else {
         cout << "\nSecond Innings Ended!!\n\n";
        
         cout <<"Player 2 Score is "<< second <<endl;
@@ -354,50 +417,37 @@ void start_game()
         cout << "\n\n";
        
         cout << "RESULT";
-        if(first>second) 
-        {
-           
+
+        if(first>second) {
             cout << "\n\n    Player 1 Won!!";
-        }
-        else if(first == second) 
-        {
-            
+        } else if(first == second)  {
             cout << "\n\nMatch Draw!!";
-        }
-        else 
-        {
-            
+        } else {
             cout << "\n\n    Player 2 Won!!";
         }
     }
     
     cout <<"\n\n=============================================================================================================";
-   
-    cout << "\n\nPress '0' to return to main menu....";
-    
+
+    delete_cricketer_array(user_cricketers_arr, user_num_cricketers);
+    delete_cricketer_array(ai_cricketers_arr, ai_num_cricketers);
 }
-int main()
-{
-    while(1)
-    {
-        game_name()
+
+int main() {
+    while(1) {
+        game_name();
         main_menu();
         char main_menu_choice, return_to_menu;
         cin >> main_menu_choice;
-        if(main_menu_choice == '1')
-        {
-            start_game();       
+        if(main_menu_choice == '1') {
+            start_game();
             cout << "Press any key to return to main menu...";
             cin >> return_to_menu;
-        }
-        else if(main_menu_choice == '2')
-        {
+        } else if(main_menu_choice == '2') {
             instructions();
             cout << "Press any key to return to main menu...";
             cin >> return_to_menu;
-        }
-        else
-        {
+        } else {
             break;
         }    
     }
